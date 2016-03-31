39421db291ddb25666ee0361f90885688051f3a9
sven <svenmaster89@gmail.com>
2016-03-31 17:04:32 +0200

    Initial commit

---
+++ src/UdpServer.java
@@ -0,0 +1,51 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UdpServer {
+    private int port;
+
+    public UdpServer(int port) {
+        this.port = port;
+        System.out.println(getClass().getName()+" ==> Server started. Listening on port "+port);
+    }
+    public void startServer() {
+        try {
+            String received;
+            String local_ip;
+            DatagramSocket serverSocket = new DatagramSocket(port);
+            byte[] receiveData = new byte[256];
+            byte[] sendData;
+
+            System.out.println(getClass().getName()+" ==> My name is "+InetAddress.getLocalHost().getHostName());
+
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+
+                received = new String(receivePacket.getData()).trim();
+                local_ip = InetAddress.getLocalHost().getHostAddress();
+
+                System.out.println(getClass().getName()+" ==> Received UDP packet from "+receivePacket.getAddress());
+                System.out.println(getClass().getName()+" ==> Message: "+received);
+
+                if(received.equals(InetAddress.getLocalHost().getHostName())) {
+                    System.out.println(getClass().getName()+" ==> Valid request! Sending my ip: "+local_ip);
+                    InetAddress IPAddress = receivePacket.getAddress();
+                    int clientPort = receivePacket.getPort();
+
+                    sendData = local_ip.getBytes();
+
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, clientPort);
+                    serverSocket.send(sendPacket);
+                } else {
+                    System.out.println(getClass().getName()+" ==> Invalid request! Dropping packet");
+                }
+            }
+        } catch (Exception e) {
+            System.err.println(getClass().getName()+" ==> Exception!");
+            e.printStackTrace();
+        }
+    }
+}
+
